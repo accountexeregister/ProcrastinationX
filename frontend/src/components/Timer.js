8d2b4836c0f02e0f64a0619419d9a28538803199
= <yeohchongyi2@outlook.com>
2023-04-25 21:52:19 +0800

    Add pausebutton icon to timer

--- frontend/src/components/Timer.js
+++ frontend/src/components/Timer.js
@@ -1,13 +1,15 @@
 import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
+import PauseButton from './PauseButton';
 import PlayButton from './PlayButton';
 
 const Timer = () => {
     return (
         <div>
             <CircularProgressbar value={50} text={"50%"}/>;
-            <div>
+            <div style={{marginTop: "20px;"}}>
                 <PlayButton />
+                <PauseButton />
             </div>
         </div>
     )
