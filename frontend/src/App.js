ad0056bde0d3e6cde775c8698ec17f2cad3d10cf
= <yeohchongyi2@outlook.com>
2023-04-25 20:51:21 +0800

    Add register, login buttons, frontend proxy

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,9 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import { useState, useEffect } from 'react';
 import Login from './components/Login';
+import Register from './components/Register';
 
 const App = () => {
   const [user, setUser] = useState(null);
+  const [isLogin, setIsLogin] = useState(false);
+  const [isRegister, setIsRegister] = useState(false);
 
   useEffect(() => {
     const loggedUserJSON = window.localStorage.getItem("loggedProcXUser");
@@ -13,11 +16,24 @@ const App = () => {
       setUser(user);
     }
   }, []);
-  return (
-    <>
-    {user === null && <Login setUser={setUser} />}
-    </>
-  );
+
+  if (isLogin) {
+    return <Login setUser={setUser}/>;
+  } else if (isRegister) {
+    return <Register/>;
+  } else {
+    return (
+      <>
+      {user && <div>Logged in as {user.username}</div>}
+      <button onClick={() => setIsLogin(true)}>
+        Login
+      </button>
+      <button onClick={() => setIsRegister(true)}>
+        Register
+      </button>
+      </>
+    );
+  }
 }
 
 export default App;
