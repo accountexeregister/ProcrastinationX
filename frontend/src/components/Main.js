320c4264baf68cbc26aabe69a0ad25c2b222c6b4
= <yeohchongyi2@outlook.com>
2023-04-25 22:57:18 +0800

    Add settingscontext, modify settings sliders

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -1,13 +1,24 @@
 import Settings from "./Settings";
 import Timer from "./Timer";
 import { useState } from "react";
+import SettingsContext from "./SettingsContext";
 
 const Main = () => {
 
     const [settingsVisible, setSettingsVisible] = useState(true);
+    const [workMinutes, setWorkMinutes] = useState(45);
+    const [breakMinutes, setBreakMinutes] = useState(15);
+
     return (
         <main>
-            {settingsVisible ? <Settings/> : <Timer/>}
+            <SettingsContext.Provider value={{
+                workMinutes,
+                breakMinutes,
+                setWorkMinutes,
+                setBreakMinutes,
+            }}>
+                {settingsVisible ? <Settings/> : <Timer/>}
+            </SettingsContext.Provider>
         </main>
     )
 }
--- frontend/src/components/Settings.js
+++ frontend/src/components/Settings.js
@@ -1,24 +1,29 @@
 import ReactSlider from "react-slider";
 import "../css-components/slider.css";
+import { useContext } from "react";
+import SettingsContext from "./SettingsContext";
 
 const Settings = () => {
+    const settings = useContext(SettingsContext);
     return (
         <div style={{textAlign: "left"}}>
-            <label>work minutes:</label>
+            <label>work minutes: {settings.workMinutes}:00</label>
             <ReactSlider
                 className={"slider"}
                 thumbClassName={"thumb"}
                 trackClassName={"track"}
-                value={45}
+                value={settings.workMinutes}
+                onChange={newVal => settings.setWorkMinutes(newVal)}
                 min={1}
                 max={120}
             />
-            <label>break minutes:</label>
+            <label>break minutes: {settings.breakMinutes}:00</label>
             <ReactSlider
                 className={"slider green"}
                 thumbClassName={"thumb"}
                 trackClassName={"track"}
-                value={45}
+                value={settings.breakMinutes}
+                onChange={newVal => settings.setBreakMinutes(newVal)}
                 min={1}
                 max={120}
             />
